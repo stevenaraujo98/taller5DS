acb73e3faa2d1ebbbeac05f92710af53d5dfb7bd
RogerAviles22 <rogiavil@espol.edu.ec>
2019-07-29 10:48:18 -0500

    Agregada paquete ChainOfR y Manejador

---
+++ src/ChainOfResponsability/Manejador.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChainOfResponsability;
+
+/**
+ *
+ * @author CltControl
+ */
+public interface Manejador {
+    Manejador manejador (int n, double denominacion);
+    void setNext(Manejador m);
+    boolean retirar(double monto);
+    boolean depositar(int n, double denominacion);
+}
