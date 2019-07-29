e2257d44fff08524b9da0abe13803aca8a83a516
bryantutiven2 <bryan.tutiven.2@outlook.com>
2019-07-29 10:33:50 -0500

    Creacion de interfaz manejador y de la clase manejadorDinero

---
+++ src/ChainOfResponsability/ManejadorDinero.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChainOfResponsability;
+
+/**
+ *
+ * @author CltControl
+ */
+public class ManejadorDinero implements Manejador{
+    
+    private Manejador next;
+    protected int cantidad;
+    protected double denominacion;
+
+    @Override
+    public Manejador manejador(int n, double denominacion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setNext(Manejador manjejador) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean retirar(double monto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean depositar(int n, double denominacion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
