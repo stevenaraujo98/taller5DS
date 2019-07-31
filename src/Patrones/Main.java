4f05d53ff20377293eb1ad08ababb764b346525f
Luis Eduardo Salazar Castro <luis8e@hotmail.com>
2019-07-31 18:49:23 -0500

    modificacion al main para que ya se pueda ejecutar el programa de manera correcta

--- src/Patrones/Main.java
+++ src/Patrones/Main.java
@@ -18,7 +18,7 @@ public class Main
         // Crear un único cajero Automático de dólares con 100 billetes de 20, 100 de 10, 
         // 10 monedas de 0.50, 10 de 0.25 y 1000 de 0.05
         
-        ManejadorDinero b20 = new ManejadorDinero(100, 20);
+        /*ManejadorDinero b20 = new ManejadorDinero(100, 20);
         ManejadorDinero b10 = new ManejadorDinero(100, 10);
         ManejadorDinero b_50 = new ManejadorDinero(10, 0.50);
         ManejadorDinero b_25 = new ManejadorDinero(10, 0.25);
@@ -42,7 +42,7 @@ public class Main
             d1= r.nextLine();
             System.out.print("Ingrese cantidad de billete a depositar: ");
             n1= r.nextInt();
-        }while(b20.depositar((int) n1, Double.parseDouble(d1))!=false);
+        }while(b20.depositar((int) n1, Double.parseDouble(d1))!=false);*/
         // Crear 10 cuentas nuevas en dólares locale.US con un saldo inicial entre 100.00 y 1000.00 USD cada una.
         Cuenta[] lc=new Cuenta[10]; 
         for(int i=0;i<10;i++){
@@ -60,6 +60,18 @@ public class Main
         }else{
             c=lc[id];
             // Mostrar el menú para realizar transacciones en el cajero automático
+            ManejadorDinero b20 = new ManejadorDinero(100, 20);
+            ManejadorDinero b10 = new ManejadorDinero(100, 10);
+            ManejadorDinero b_50 = new ManejadorDinero(10, 0.50);
+            ManejadorDinero b_25 = new ManejadorDinero(10, 0.25);
+            ManejadorDinero b_05 = new ManejadorDinero(1000, 0.05); 
+            AtmEC atm = AtmEC.getInstance();
+            System.out.println("Ingresando manejadores");
+            atm.addManejador(b20);
+            atm.addManejador(b10);
+            atm.addManejador(b_50);
+            atm.addManejador(b_25);
+            atm.addManejador(b_05);
             AtmEC.transaction(c);
         }
         
